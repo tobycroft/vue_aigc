27ae56bb4aeda4e1f23c40738ecba31470ec8eb9
tuuz <silverhawk@foxmail.com>
2024-03-30 01:52:30 +0800

    TuuzPC

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -12,11 +12,7 @@
 
     <v-sheet width="100%" class="mx-auto mt-6" v-show="show1">
       <v-form @submit.prevent>
-        <v-text-field autofocus
-                      v-model="qq"
-                      :rules="rules"
-                      label="这里输入你的QQ号码"
-        ></v-text-field>
+        <v-text-field autofocus v-model="username" :rules="rules" label="这里输入你的用户名"></v-text-field>
         <v-btn type="submit" block class="mt-0" color="blue" @click="gotonext">下一步</v-btn>
         <v-btn type="submit" block class="mt-4" color="grey" @click="clearout">清空数据</v-btn>
       </v-form>
@@ -24,29 +20,18 @@
 
 
     <v-slide-y-transition>
-      <v-card v-show="show2"
-              class="py-8 px-6 text-center mx-auto ma-4"
-              elevation="12"
-
-              max-width="100%"
-              width="100%"
-      >
-        <h3 class="text-h6 mb-4">填写你QQ的登录码</h3>
-
+      <v-card v-show="show2" class="py-8 px-6 text-center mx-auto ma-4" elevation="12" max-width="100%" width="100%">
+        <h3 class="text-h6 mb-4">填写密码</h3>
         <div class="text-body-2">
-          <v-col>不是你的QQ密码！！！</v-col>
-          <v-col>登录码请通过已在线的Acfur机器人获取</v-col>
-          <v-col>向机器人发送“acfur登录”，并等待机器人回复验证码给你</v-col>
         </div>
 
         <v-sheet color="surface">
-          <v-otp-input
-            v-model="password"
-            type="text"
-            length="8"
-            variant="solo-filled"
-            max-width="400"
-          ></v-otp-input>
+          <v-text-field
+              v-model="password"
+              type="password"
+              label="密码"
+              max-width="400"
+          ></v-text-field>
         </v-sheet>
 
         <v-btn @click="login"
@@ -76,6 +61,7 @@
 <script>
 import Net from "@/plugins/Net";
 import TokenModel from "@/model/TokenModel";
+import md5 from "md5";
 
 export default {
   beforeCreate() {
@@ -84,38 +70,38 @@ export default {
     }
   },
   created() {
-    this.qq = localStorage.getItem("qq")
+    this.username = localStorage.getItem("username")
   },
   data: () => ({
     show1: true,
     show2: false,
-    qq: '',
+    username: '',
     password: '',
     rules: [
       value => {
         if (value) return true
-        return '这里填写的应该是你的QQ号码，而不是你机器人的QQ号码'
+        return '这里填写的应该是你的用户名，而不是你机器人的用户名'
       },
     ],
   }),
   methods: {
     gotonext() {
-      if (this.qq.length > 5) {
+      if (this.username.length > 1) {
         this.show1 = false;
         this.show2 = true
-        localStorage.setItem("qq", this.qq)
+        localStorage.setItem("username", this.username)
       }
     },
     clearout() {
       this.show2 = false
       this.show1 = true
-      this.qq = ''
-      localStorage.removeItem('qq')
+      this.username = ''
+      localStorage.removeItem('username')
     },
     async login() {
-      var ret = await new Net("/v1/index/login/login").PostFormData({
-        qq: this.qq,
-        password: this.password
+      var ret = await new Net("/v1/user/auth/login").PostFormData({
+        username: this.username,
+        password: md5(this.password)
       })
       if (ret.isSuccess) {
         TokenModel.Api_set_uidAndToken(ret.data["uid"], ret.data["token"])
@@ -126,4 +112,3 @@ export default {
 }
 
 </script>
-
