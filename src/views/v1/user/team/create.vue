194bce3d45ba98f3797b32a1000b91d555146152
tuuz <silverhawk@foxmail.com>
2024-03-30 04:30:55 +0800

    TuuzPC

--- src/views/v1/user/team/create.vue
+++ src/views/v1/user/team/create.vue
@@ -4,8 +4,11 @@
       <v-card-title>创建团队信息</v-card-title>
       <v-card-text>
         <v-form @submit.prevent="submitTeamInfo">
-          <v-text-field v-model="teamName" label="团队名称"></v-text-field>
-          <v-btn type="submit" color="primary">提交</v-btn>
+          <v-text-field v-model="name" label="团队名称"></v-text-field>
+          <v-tab>
+            <v-btn type="submit" color="primary">提交</v-btn>
+            <v-btn @click="goBack" color="primary">返回</v-btn>
+          </v-tab>
         </v-form>
       </v-card-text>
     </v-card>
@@ -18,24 +21,25 @@ import Net from "@/plugins/Net";
 export default {
   data() {
     return {
-      teamName: '', // 存储输入框中的团队名称
+      name: '', // 存储输入框中的团队名称
     };
   },
   methods: {
     async submitTeamInfo() {
       // 发送创建团队信息的请求
-      const formData = new FormData();
-      formData.append('name', this.teamName);
-
-      const ret = await new Net("/v1/team/info/create").PostFormData(formData);
+      const ret = await new Net("/v1/team/info/create").PostFormData(this.$data);
       if (ret.code === 0) {
         // 处理成功响应
         console.log("团队信息创建成功");
+        this.goBack();
       } else {
         // 处理失败响应
         console.error(ret.echo);
       }
     },
+    goBack() {
+      this.$router.push("/v1/user/team");
+    }
   },
 };
 </script>
