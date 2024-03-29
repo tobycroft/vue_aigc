9a0c605e7c1005a701c07383abcfce02cf7144f2
tuuz <silverhawk@foxmail.com>
2024-03-30 04:12:09 +0800

    TuuzPC

--- src/router/route.js
+++ src/router/route.js
@@ -1,67 +1,67 @@
 // Composables
 import {createRouter, createWebHistory} from 'vue-router'
 
-const routess = [
-    {
-        path: '/',
-        // component: () => import('@/layouts/default/Default.vue'),
-        children: [
-            {
-                path: '',
-                // route level code-splitting
-                // this generates a separate chunk (Home-[hash].js) for this route
-                // which is lazy-loaded when the route is visited.
-                // component: () => import('@/views/Home.vue'),
-                component: () => import('@/views/user/login.vue'),
-            },
-
-        ],
-    },
-    {
-        path: '/user/login',
-        component: () => import('@/views/user/login.vue'),
-    },
-    {
-        path: '/center',
-        component: () => import('@/views/center.vue'),
-    },
-    {
-        path: '/v1/bot',
-        component: () => import('@/views/v1/bot.vue'),
-    },
-    {
-        path: '/v1/group',
-        component: () => import('@/views/v1/group.vue'),
-    },
-    {
-        path: '/v1/index',
-        component: () => import('@/views/v1/index.vue'),
-    },
-    {
-        path: '/v1/user',
-        component: () => import('@/views/v1/user.vue'),
-    },
-    {
-        path: '/v1/bot/detail',
-        component: () => import('@/views/v1/bot/subpage/detail.vue'),
-    },
-    {
-        path: '/v1/bot/edit',
-        component: () => import('@/views/v1/bot/subpage/edit.vue'),
-    },
-    {
-        path: '/v1/bot/setting',
-        component: () => import('@/views/v1/bot/subpage/setting.vue'),
-    },
-    {
-        path: '/v1/group/edit',
-        component: () => import('@/views/v1/group/subpage/edit.vue'),
-    },
-    {
-        path: '/v1/index/botrequest/edit',
-        component: () => import('@/views/v1/index/botRequest/add.vue'),
-    },
-]
+// const routess = [
+//     {
+//         path: '/',
+//         // component: () => import('@/layouts/default/Default.vue'),
+//         children: [
+//             {
+//                 path: '',
+//                 // route level code-splitting
+//                 // this generates a separate chunk (Home-[hash].js) for this route
+//                 // which is lazy-loaded when the route is visited.
+//                 // component: () => import('@/views/Home.vue'),
+//                 component: () => import('@/views/user/login.vue'),
+//             },
+//
+//         ],
+//     },
+//     {
+//         path: '/user/login',
+//         component: () => import('@/views/user/login.vue'),
+//     },
+//     {
+//         path: '/center',
+//         component: () => import('@/views/center.vue'),
+//     },
+//     {
+//         path: '/v1/bot',
+//         component: () => import('@/views/v1/bot.vue'),
+//     },
+//     {
+//         path: '/v1/group',
+//         component: () => import('@/views/v1/group.vue'),
+//     },
+//     {
+//         path: '/v1/index',
+//         component: () => import('@/views/v1/index.vue'),
+//     },
+//     {
+//         path: '/v1/user',
+//         component: () => import('@/views/v1/user.vue'),
+//     },
+//     {
+//         path: '/v1/bot/detail',
+//         component: () => import('@/views/v1/bot/subpage/detail.vue'),
+//     },
+//     {
+//         path: '/v1/bot/edit',
+//         component: () => import('@/views/v1/bot/subpage/edit.vue'),
+//     },
+//     {
+//         path: '/v1/bot/setting',
+//         component: () => import('@/views/v1/bot/subpage/setting.vue'),
+//     },
+//     {
+//         path: '/v1/group/edit',
+//         component: () => import('@/views/v1/group/subpage/edit.vue'),
+//     },
+//     {
+//         path: '/v1/index/botrequest/edit',
+//         component: () => import('@/views/v1/index/botRequest/add.vue'),
+//     },
+// ]
 
 const routes = [
     {
