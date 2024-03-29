a619a7fe4f06a6729180e6a74e494edcbbe98fe3
tuuz <silverhawk@foxmail.com>
2024-03-30 02:40:22 +0800

    TuuzPC

--- src/views/v1/user/info.vue
+++ src/views/v1/user/info.vue
@@ -1,35 +1,59 @@
 <template>
-  <!-- 显示 QQ 号码 -->
-  <v-row justify="center">
-    <v-col cols="12">
-      <v-card>
-        <v-card-title>个人信息</v-card-title>
-        <v-card-text>
-          <v-row>
-            <v-col cols="12">
-              <v-card>
-                <v-card-text>
-                  <strong>QQ号码：</strong> {{ formData.qq }}
-                </v-card-text>
-                <v-card-text>
-                  <strong>加入时间：</strong> {{ formData.date }}
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-card-text>
-        <v-card-actions>
-          <!-- 退出按钮 -->
-          <v-btn @click="logout" color="primary">退出</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-card>
+      <v-card-title>个人信息</v-card-title>
+      <v-card-text>
+        <v-row>
+          <v-col cols="12">
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-card>
+                    <v-card-text>
+                      <strong>用户ID：</strong> {{ formData.uid }}
+                    </v-card-text>
+                  </v-card>
+                </v-col>
+                <v-col cols="12">
+                  <v-card>
+                    <v-card-text>
+                      <strong>集团头像：</strong> {{ formData.group_img || '暂无' }}
+                    </v-card-text>
+                  </v-card>
+                </v-col>
+                <v-col cols="12">
+                  <v-card>
+                    <v-card-text>
+                      <strong>集团名称：</strong> {{ formData.group_name || '暂无' }}
+                    </v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12">
+                  <v-card>
+                    <v-card-text>
+                      <strong>加入时间：</strong> {{ formattedDate }}
+                    </v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-btn variant="tonal" class="flex-grow-1" size="x-large" @click="logout" color="primary" large>退出</v-btn>
+              </v-row>
+
+            </v-container>
+          </v-col>
+        </v-row>
+
+      </v-card-text>
+
+    </v-card>
+
+  </v-container>
 </template>
 
-
 <script>
-
 import Net from "@/plugins/Net";
 import moment from "moment";
 import TokenModel from "@/model/TokenModel";
@@ -37,28 +61,31 @@ import TokenModel from "@/model/TokenModel";
 export default {
   data() {
     return {
-      formData: {}, // 替换为实际的 QQ 号码
+      formData: {}, // 存储接口返回的数据
     };
   },
+  computed: {
+    formattedDate() {
+      // 格式化日期
+      return moment(this.formData.date).format('YYYY-MM-DD HH:mm:ss');
+    }
+  },
   methods: {
-    // 退出按钮点击事件
-    logout() {
-      // 处理退出逻辑，可以调用接口等操作
-      TokenModel.Api_clear_uidAndToken()
-      this.$router.push('/v1/login')
-    },
     async fetchData() {
       // 发送获取个人信息的请求
       const ret = await new Net("/v1/user/info/get").Get();
-      if (ret.isSuccess) {
-        // 将获取到的 QQ 号码更新到数据中
-        const data = ret.data
-        data['date'] = moment(data['date']).format('YYYY-MM-DD HH:mm:ss');
-        this.formData = ret.data
+      if (ret.code === 0) {
+        // 更新 formData
+        this.formData = ret.data;
       } else {
-        // 处理请求失败的情况，可以显示错误信息等
+        // 处理请求失败的情况
         console.error(ret.echo);
       }
+    },
+    logout() {
+      // 处理退出逻辑
+      TokenModel.Api_clear_uidAndToken();
+      this.$router.push('/v1/login');
     },
   },
   mounted() {
--- src/views/v1/user/team.vue
+++ src/views/v1/user/team.vue
@@ -1,69 +1,53 @@
 <template>
-  <!-- 显示 QQ 号码 -->
-  <v-row justify="center">
-    <v-col cols="12">
-      <v-card>
-        <v-card-title>个人信息</v-card-title>
-        <v-card-text>
-          <v-row>
-            <v-col cols="12">
-              <v-card>
-                <v-card-text>
-                  <strong>QQ号码：</strong> {{ formData.qq }}
-                </v-card-text>
-                <v-card-text>
-                  <strong>加入时间：</strong> {{ formData.date }}
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-card-text>
-        <v-card-actions>
-          <!-- 退出按钮 -->
-          <v-btn @click="logout" color="primary">退出</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-card>
+      <v-card-title>团队列表</v-card-title>
+      <v-card-text>
+        <v-list>
+          <v-list-item v-for="team in teamList" :key="team.id">
+            <v-list-item-content>
+              <v-list-item-title>{{ team.team_info.name }}</v-list-item-title>
+              <v-list-item-subtitle v-if="team.nickname">{{ team.nickname }}</v-list-item-subtitle>
+              <v-list-item-subtitle>{{ team.role }}</v-list-item-subtitle>
+              <v-list-item-subtitle>{{ formattedDate(team.date) }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
-
 <script>
-
 import Net from "@/plugins/Net";
 import moment from "moment";
-import TokenModel from "@/model/TokenModel";
 
 export default {
   data() {
     return {
-      formData: {}, // 替换为实际的 QQ 号码
+      teamList: [], // 存储团队列表数据
     };
   },
   methods: {
-    // 退出按钮点击事件
-    logout() {
-      // 处理退出逻辑，可以调用接口等操作
-      TokenModel.Api_clear_uidAndToken()
-      this.$router.push('/v1/login')
-    },
-    async fetchData() {
-      // 发送获取个人信息的请求
-      const ret = await new Net("/v1/user/info/get").Get();
-      if (ret.isSuccess) {
-        // 将获取到的 QQ 号码更新到数据中
-        const data = ret.data
-        data['date'] = moment(data['date']).format('YYYY-MM-DD HH:mm:ss');
-        this.formData = ret.data
+    async fetchTeamList() {
+      // 发送获取团队列表的请求
+      const ret = await new Net("/v1/user/team/list").Get();
+      if (ret.code === 0) {
+        // 更新 teamList
+        this.teamList = ret.data;
       } else {
-        // 处理请求失败的情况，可以显示错误信息等
+        // 处理请求失败的情况
         console.error(ret.echo);
       }
     },
+    formattedDate(date) {
+      // 格式化日期
+      return moment(date).format('YYYY-MM-DD HH:mm:ss');
+    }
   },
   mounted() {
-    // 在组件挂载时调用 fetchData 方法获取数据
-    this.fetchData();
+    // 在组件挂载时调用 fetchTeamList 方法获取团队列表数据
+    this.fetchTeamList();
   },
 };
 </script>
