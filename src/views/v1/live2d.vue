7aaf5f9d9d1bbc0b54b20b0dd3c3fdc4c24525b9
tuuz <silverhawk@foxmail.com>
2024-03-30 23:46:10 +0800

    TuuzPC

--- src/views/v1/live2d.vue
+++ src/views/v1/live2d.vue
@@ -1,6 +1,7 @@
 <script setup>
 
 import Topheader from "@/components/center/header/topheader.vue";
+import "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js";
 </script>
 
 <template>
