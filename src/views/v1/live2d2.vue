42c4673d677051358ae8067c26d982b4ad18bffe
tuuz <silverhawk@foxmail.com>
2024-03-31 01:03:57 +0800

    TuuzPC

---
+++ src/views/v1/live2d2.vue
@@ -0,0 +1,28 @@
+<script setup>
+import 'https://cdn.bootcss.com/jquery/2.2.4/jquery.min.js'
+import '@/assets/live2d/js/live2d.js';
+import '@/assets/live2d/js/message.js';
+import Topheader from "@/components/center/header/topheader.vue";
+
+var message_Path = 'live2d/'
+var home_Path = 'https://localhost/'
+
+loadlive2d("live2d", "live2d/model/tia/model.json");
+</script>
+
+<template>
+  <topheader></topheader>
+  <!--  <head>-->
+  <!--    <meta charset="utf-8">-->
+  <!--    <title>Live2D！把可爱的看板娘扑捉到你的博客去吧！</title>-->
+  <!--  </head>-->
+  <div id="landlord">
+    <div class="message" style="opacity:0"></div>
+    <canvas id="live2d" width="280" height="250" class="live2d"></canvas>
+    <div class="hide-button">隐藏</div>
+  </div>
+</template>
+
+<style href="live2d/css/live2d.css">
+
+</style>
