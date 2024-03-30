94e00068ae040d8dd7358bb5210c48b954790bb6
tuuz <silverhawk@foxmail.com>
2024-03-30 17:03:17 +0800

    TuuzPC

--- src/views/v1/user/team/edit.vue
+++ src/views/v1/user/team/edit.vue
@@ -3,6 +3,8 @@
     <v-card-title>编辑团队信息</v-card-title>
     <v-form @submit.prevent="updateTeamInfo">
       <v-text-field v-model="name" label="团队名称"></v-text-field>
+      <v-text-field v-model="content" label="团队简介"></v-text-field>
+      <v-text-field v-model="prefix" label="key标头"></v-text-field>
       <!-- 如果还有其他要编辑的字段，可以在这里添加 -->
       <div class="d-flex flex-column">
         <v-btn size="large" type="submit" color="primary" class="mt-4">保存</v-btn>
@@ -19,6 +21,8 @@ export default {
   data() {
     return {
       name: '', // 存储输入框中的团队名称
+      content: '', // 存储输入框中的团队名称
+      prefix: '', // 存储输入框中的团队名称
       team: this.$route.query // 获取团队ID
     };
   },
@@ -28,7 +32,9 @@ export default {
       const ret = await new Net(`/v1/user/team/get`).PostFormData(this.team);
       if (ret.code === 0) {
         // 填充团队信息到表单中
-        this.name = ret.data.name;
+        this.name = ret.data.team_info.name;
+        this.content = ret.data.team_info.content;
+        this.prefix = ret.data.team_info.prefix;
         // 如果还有其他字段，也可以在这里填充
       } else {
         console.error(ret.echo);
@@ -37,9 +43,10 @@ export default {
     async updateTeamInfo() {
       // 构造要发送的数据
       const payload = {
-        id: this.teamId,
-        name: this.name
-        // 如果还有其他字段需要编辑，可以在这里添加
+        id: this.team.team_id,
+        name: this.name,
+        img: "",
+        content: this.content
       };
 
       // 发送更新团队信息的请求
