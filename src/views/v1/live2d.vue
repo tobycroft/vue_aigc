1cbb1472d58925188c2520d67ad8779cde26fdc1
tuuz <silverhawk@foxmail.com>
2024-03-30 18:40:22 +0800

    TuuzPC

--- src/views/v1/live2d.vue
+++ src/views/v1/live2d.vue
@@ -1,43 +1,11 @@
-<template>
-  <v-app>
-    <!-- 顶部导航栏 -->
-    <Topheader></Topheader>
-    <v-tabs v-model="tab" @click="changetab"
-            fixed-tabs
-            bg-color="indigo-darken-2"
-    >
-      <v-tab>添加我的机器人</v-tab>
+<script setup>
 
-    </v-tabs>
-    <!-- 主页面内容区域 -->
-    <BotRequest v-if="tab===0"></BotRequest>
-  </v-app>
-
-</template>
-
-
-<script>
-
-import {defineComponent} from "vue";
-import BotRequest from "@/views/v1/index/BotRequest.vue";
 import Topheader from "@/components/center/header/topheader.vue";
-
-export default defineComponent({
-  components: {Topheader, BotRequest},
-  computed: {},
-  mounted() {
-  },
-  data: () => ({
-    tab: '',
-  }),
-  methods: {
-    changetab() {
-      // console.log(this.tab)
-    },
-  },
-})
-
 </script>
+
+<template>
+  <Topheader></Topheader>
+</template>
 
 <style scoped>
 
