574f6a4687c31cdf350a9065b940f566a27ffb10
tuuz <silverhawk@foxmail.com>
2024-03-31 00:38:05 +0800

    TuuzPC

--- src/views/v1/live2d.vue
+++ src/views/v1/live2d.vue
@@ -1,13 +1,78 @@
-<script setup>
-
-import Topheader from "@/components/center/header/topheader.vue";
-import "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js";
-</script>
-
 <template>
-  <Topheader></Topheader>
+  <topheader></topheader>
+  <!-- 使用示例 -->
+  <div style="margin: 2px 0">注意：如果有时候按钮不能点击，是因为模型 zIndex 挡住了按钮。</div>
+  <!--  <div>-->
+  <!--    <span>position style：</span>-->
+  <!--    <button @click="style = 'position: fixed; top: calc(50% - 250px); left: calc(50% - 200px)'">呆在中间</button>-->
+  <!--    <button @click="style = 'position: fixed; top: 0; left: 0; zIndex: 1'">呆在左上角</button>-->
+  <!--    <button @click="style = 'position: fixed; top: 0; right: 0; zIndex: 1'">呆在右上角</button>-->
+  <!--    <button @click="style = 'position: fixed; bottom: 0; left: 0; zIndex: 1'">呆在左下角</button>-->
+  <!--    <button @click="style = 'position: fixed; bottom: 0; right: 0;'">呆在右下角</button>-->
+  <!--    <button @click="style = ''">跟随流</button>-->
+  <!--  </div>-->
+  <div>
+    <span>direction：</span>
+    <button @click="direction = 'right'">呆在右边</button>
+    <button @click="direction = 'left'">呆在左边</button>
+  </div>
+  <div>
+    <span>size：</span>
+    <button @click="size = size - 100; width = width - 100; height = height - 100;">减小</button>
+    <button @click="size = size + 100; width = width + 100; height = height + 100;">增大</button>
+  </div>
+  <!-- 可尝试一个页面上多个模型： -->
+  <!-- <live2d
+    :model="[1, 53]"
+    :direction="direction"
+    :width="width"
+    :height="height"
+    customId="vue-live2d-mian-2"
+  ></live2d> -->
+  <live2d
+      :style="style"
+      :model="['Potion-Maker/Pio', 'school-2017-costume-yellow']"
+      :direction="direction"
+      :size="size"
+  ></live2d>
 </template>
 
-<style scoped>
+<script>
+/*
+ * 项目中引用包时，将 import 内容替换
+ * import live2d from 'vue-live2d'
+ */
+import Topheader from "@/components/center/header/topheader.vue";
+import live2d from 'vue-live2d'
 
+export default {
+  name: 'App',
+  components: {
+    Topheader,
+    live2d
+  },
+  data() {
+    return {
+      direction: 'right',
+      style: 'position: fixed; bottom: 0; right: 0;',
+      width: 800,
+      height: 800,
+      size: 600,
+      apiPath:"https://live2d.fghrsh.net/api/",
+      tips: {
+        mouseover: [{
+          selector: '.vue-live2d',
+          texts: ['这样可以修改默认语句']
+        }]
+      }
+    }
+  },
+  created() {
+    // tips 未使用，可自行在组件绑定此参数，注意此参数只会初始化一次
+    // this.tips = this.customTips
+  }
+}
+</script>
+
+<style>
 </style>
