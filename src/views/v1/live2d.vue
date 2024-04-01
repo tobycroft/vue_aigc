7d76862737e2e7d8e291656dbb62a9198ec7c01b
tuuz <silverhawk@foxmail.com>
2024-04-01 14:36:29 +0800

    TuuzPC

--- src/views/v1/live2d.vue
+++ src/views/v1/live2d.vue
@@ -64,10 +64,6 @@ export default {
     }
   },
   created() {
-    // this.components.live2d.methods.showMessage("aaa", 2000)
-    // live2d.methods.showMessage("aaa", 2000)
-    // tips 未使用，可自行在组件绑定此参数，注意此参数只会初始化一次
-    // this.tips = this.customTips
   },
   methods: {
     updateMessage() {
