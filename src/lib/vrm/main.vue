cb7b5406d7a18bce9c6230f0b20c5a94c7f2f174
tuuz <silverhawk@foxmail.com>
2024-04-01 15:26:58 +0800

    TuuzPC

---
+++ src/lib/vrm/main.vue
@@ -0,0 +1,99 @@
+<script>
+
+const Viewer = await import("https://cdn.jsdelivr.net/gh/josephrocca/ChatVRM-js@v0.0.28/features/vrmViewer/viewer.js").then(m => m.Viewer);
+let viewer = null;
+
+export default {
+  name: 'App',
+  components: {
+    // Live,
+  },
+  props: {
+    aigcUrl: {
+      default: 'https://aigc.aerofsx.com:444',
+      type: String
+    },
+    sdkUrl: {
+      default: '',
+      type: String,
+    },
+    model: {
+      default: () => ['Potion-Maker/Pio', 'school-2017-costume-yellow'],
+      type: Array
+    },
+    homePage: {
+      default: 'https://ai.aerofsx.com:444',
+      type: String
+    },
+    width: {
+      default: 0,
+      type: Number
+    },
+    height: {
+      default: 0,
+      type: Number
+    },
+    size: {
+      default: 255,
+      type: Number
+    },
+    uid: {
+      default: 0,
+    },
+    token: {
+      default: "",
+    }
+  },
+  data() {
+    return {
+      message: "",
+    }
+  },
+  async mounted() {
+
+    viewer = new Viewer();
+
+    const canvas = document.createElement("canvas");
+    canvas.height = this.height ? this.height : 300;
+    canvas.width = this.width ? this.width : 300;
+    //attach the canvas to the DOM which id is "canvas"
+    document.getElementById("canvas").appendChild(canvas);
+    // document.body.appendChild(canvas);
+    // canvas.style.cssText = "width:300px; height:300px; display:block;";
+    // document.body.style.cssText = "margin:0; padding:0;";
+
+    viewer.setup(canvas);
+
+    await viewer.loadVrm("https://raw.githubusercontent.com/josephrocca/ChatVRM-js/main/avatars/AvatarSample_B.vrm");
+
+    viewer.model.emoteController.playEmotion("angry"); // Valid expressions: neutral, happy, angry, sad, relaxed
+
+    // await viewer.model.loadAnimation("https://cdn.jsdelivr.net/gh/josephrocca/ChatVRM-js@v0.0.28/OpenCharacters/animations/silly_dancing.fbx");
+
+    // if (!navigator.userActivation?.hasBeenActive) {
+    //   await new Promise(resolve => window.addEventListener("click", resolve, {once: false}));
+    // }
+
+    // let arrayBuffer = await fetch("https://cdn.jsdelivr.net/gh/josephrocca/ChatVRM-js@v0.0.22/OpenCharacters/dummy-audio/12.mp3").then(r => r.arrayBuffer());
+    // await viewer.model.speak(arrayBuffer, {expression: "happy"});
+  },
+  methods: {
+    updateMessage() {
+
+    },
+    chatMessage() {
+
+    }
+  },
+}
+</script>
+
+<template>
+  <v-container id="canvas">
+  </v-container>
+
+</template>
+
+<style scoped>
+/* Add any necessary styles here */
+</style>
--- src/views/v1/vrm.vue
+++ src/views/v1/vrm.vue
@@ -1,16 +1,13 @@
 <script>
 import Topheader from "@/components/center/header/topheader.vue";
-import live2d from "@/lib/l2d/main.vue";
+import vrm from '@/lib/vrm/main.vue'
 
-const Viewer = await import("https://cdn.jsdelivr.net/gh/josephrocca/ChatVRM-js@v0.0.28/features/vrmViewer/viewer.js").then(m => m.Viewer);
-let viewer = null;
 
 export default {
   name: 'App',
   components: {
     Topheader,
-    live2d,
-    // Live,
+    vrm,
   },
   data() {
     return {
@@ -19,31 +16,6 @@ export default {
   },
   async mounted() {
 
-    viewer = new Viewer();
-
-    const canvas = document.createElement("canvas");
-    canvas.height = 300;
-    canvas.width = 300;
-    //attach the canvas to the DOM which id is "canvas"
-    document.getElementById("canvas").appendChild(canvas);
-    // document.body.appendChild(canvas);
-    // canvas.style.cssText = "width:300px; height:300px; display:block;";
-    // document.body.style.cssText = "margin:0; padding:0;";
-
-    viewer.setup(canvas);
-
-    await viewer.loadVrm("https://raw.githubusercontent.com/josephrocca/ChatVRM-js/main/avatars/AvatarSample_B.vrm");
-
-    viewer.model.emoteController.playEmotion("angry"); // Valid expressions: neutral, happy, angry, sad, relaxed
-
-    await viewer.model.loadAnimation("https://cdn.jsdelivr.net/gh/josephrocca/ChatVRM-js@v0.0.28/OpenCharacters/animations/silly_dancing.fbx");
-
-    if (!navigator.userActivation?.hasBeenActive) {
-      await new Promise(resolve => window.addEventListener("click", resolve, {once: true}));
-    }
-
-    let arrayBuffer = await fetch("https://cdn.jsdelivr.net/gh/josephrocca/ChatVRM-js@v0.0.22/OpenCharacters/dummy-audio/12.mp3").then(r => r.arrayBuffer());
-    await viewer.model.speak(arrayBuffer, {expression: "happy", volume: 0});
   },
   methods: {
     updateMessage() {
@@ -59,6 +31,10 @@ export default {
 <template>
   <Topheader></Topheader>
   <v-container>
+    <vrm
+    :height="300"
+    :width="300"
+    ></vrm>
     <v-card-title>L2D语音+显示</v-card-title>
     <v-form @submit.prevent="updateMessage">
       <v-text-field v-model="message" label="你想让L2D说什么"></v-text-field>
@@ -74,9 +50,7 @@ export default {
       </div>
     </v-form>
   </v-container>
-  <v-card id="canvas">
 
-  </v-card>
 </template>
 
 <style scoped>
