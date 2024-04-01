a7eab33d0ba733a376d8963cf3c7a07c91052194
tuuz <silverhawk@foxmail.com>
2024-04-01 14:12:18 +0800

    TuuzPC

---
+++ dist/assets/vrm-CH0KJYh2.js
@@ -0,0 +1,15 @@
+import { T as Topheader } from "./topheader-DpsJTOFN.js";
+import { c as createBlock, o as openBlock } from "./index-BAlY9acN.js";
+import "./gobotq-CybS7j7m.js";
+import "./index-BJfEYQYk.js";
+const _sfc_main = {
+  __name: "vrm",
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return openBlock(), createBlock(Topheader);
+    };
+  }
+};
+export {
+  _sfc_main as default
+};
