a4438c32aa07cc7ca07488be510828a353881be5
tuuz <silverhawk@foxmail.com>
2024-04-01 14:07:37 +0800

    TuuzPC

---
+++ dist/assets/ssrBoot-CHNpWwPx.js
@@ -0,0 +1,19 @@
+import { s as shallowRef, e as onMounted, a as computed, bn as readonly } from "./index-o_pSkn9Q.js";
+function useSsrBoot() {
+  const isBooted = shallowRef(false);
+  onMounted(() => {
+    window.requestAnimationFrame(() => {
+      isBooted.value = true;
+    });
+  });
+  const ssrBootStyles = computed(() => !isBooted.value ? {
+    transition: "none !important"
+  } : void 0);
+  return {
+    ssrBootStyles,
+    isBooted: readonly(isBooted)
+  };
+}
+export {
+  useSsrBoot as u
+};
---
+++ dist/assets/vrm-Cpqjom7V.js
@@ -0,0 +1,15 @@
+import { T as Topheader } from "./topheader-Ds2UOJqE.js";
+import { c as createBlock, o as openBlock } from "./index-o_pSkn9Q.js";
+import "./gobotq-CybS7j7m.js";
+import "./index-d8d5cF66.js";
+const _sfc_main = {
+  __name: "vrm",
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return openBlock(), createBlock(Topheader);
+    };
+  }
+};
+export {
+  _sfc_main as default
+};
