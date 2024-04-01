dabf71fc6b853bc594f6f7b487e151b1949feb55
tuuz <silverhawk@foxmail.com>
2024-04-01 15:12:33 +0800

    TuuzPC

--- src/views/v1/vrm.vue
+++ src/views/v1/vrm.vue
@@ -12,22 +12,29 @@ export default {
     live2d,
     // Live,
   },
+  data() {
+    return {
+      message: "",
+    }
+  },
   async mounted() {
 
     viewer = new Viewer();
 
     const canvas = document.createElement("canvas");
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
-    document.body.appendChild(canvas);
-    canvas.style.cssText = "width:300; height:300; display:block;";
-    document.body.style.cssText = "margin:0; padding:0;";
+    canvas.height = 300;
+    canvas.width = 300;
+    //attach the canvas to the DOM which id is "canvas"
+    document.getElementById("canvas").appendChild(canvas);
+    // document.body.appendChild(canvas);
+    // canvas.style.cssText = "width:300px; height:300px; display:block;";
+    // document.body.style.cssText = "margin:0; padding:0;";
 
     viewer.setup(canvas);
 
     await viewer.loadVrm("https://raw.githubusercontent.com/josephrocca/ChatVRM-js/main/avatars/AvatarSample_B.vrm");
 
-    viewer.model.emoteController.playEmotion("happy"); // Valid expressions: neutral, happy, angry, sad, relaxed
+    viewer.model.emoteController.playEmotion("angry"); // Valid expressions: neutral, happy, angry, sad, relaxed
 
     await viewer.model.loadAnimation("https://cdn.jsdelivr.net/gh/josephrocca/ChatVRM-js@v0.0.28/OpenCharacters/animations/silly_dancing.fbx");
 
@@ -37,15 +44,39 @@ export default {
 
     let arrayBuffer = await fetch("https://cdn.jsdelivr.net/gh/josephrocca/ChatVRM-js@v0.0.22/OpenCharacters/dummy-audio/12.mp3").then(r => r.arrayBuffer());
     await viewer.model.speak(arrayBuffer, {expression: "happy", volume: 0});
-  }
+  },
+  methods: {
+    updateMessage() {
+
+    },
+    chatMessage() {
+
+    }
+  },
 }
 </script>
 
 <template>
-  <div>
-    <Topheader></Topheader>
-    <div ref="container" style="width: 100vw; height: 100vh;"></div>
-  </div>
+  <Topheader></Topheader>
+  <v-container>
+    <v-card-title>L2D语音+显示</v-card-title>
+    <v-form @submit.prevent="updateMessage">
+      <v-text-field v-model="message" label="你想让L2D说什么"></v-text-field>
+      <div class="d-flex flex-column">
+        <v-btn size="large" type="submit" color="primary" class="mt-4">确认</v-btn>
+      </div>
+    </v-form>
+    <v-card-title>L2D语音+聊天</v-card-title>
+    <v-form @submit.prevent="chatMessage">
+      <v-text-field v-model="message" label="输入聊天内容"></v-text-field>
+      <div class="d-flex flex-column">
+        <v-btn size="large" type="submit" color="primary" class="mt-4">确认</v-btn>
+      </div>
+    </v-form>
+  </v-container>
+  <v-card id="canvas">
+
+  </v-card>
 </template>
 
 <style scoped>
