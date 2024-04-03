b6a64cceefdf1a67d6ce1acb9f703baf793678f5
tuuz <silverhawk@foxmail.com>
2024-04-03 12:54:34 +0800

    TuuzPC

---
+++ dist/assets/ssrBoot-DRMzDmAt.js
@@ -0,0 +1,19 @@
+import { s as shallowRef, e as onMounted, a as computed, bn as readonly } from "./index-DxJAMuAo.js";
+function useSsrBoot() {
+  const isBooted = shallowRef(false);
+  onMounted(() => {
+    window.requestAnimationFrame(() => {
+      isBooted.value = true;
+    });
+  });
+  const ssrBootStyles = computed(() => !isBooted.value ? {
+    transition: "none !important"
+  } : void 0);
+  return {
+    ssrBootStyles,
+    isBooted: readonly(isBooted)
+  };
+}
+export {
+  useSsrBoot as u
+};
