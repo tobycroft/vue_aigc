d7dd4d10fb42c603fd0f4b80c9f1b4e031044292
tuuz <silverhawk@foxmail.com>
2024-04-15 17:09:31 +0800

    TuuzPC

---
+++ src/app/v1/calling.vue
@@ -0,0 +1,45 @@
+<template>
+  <Topheader></Topheader>
+
+  <v-tabs v-model="tab" @click="changetab"
+          fixed-tabs
+          bg-color="indigo-darken-2"
+  >
+    <v-tab>API</v-tab>
+
+  </v-tabs>
+
+  <api v-if="tab===1"></api>
+
+
+</template>
+
+<script>
+import Topheader from "@/components/topheader.vue";
+import api from "@/app/v1/user/api.vue";
+
+export default {
+  components: {api, Topheader},
+  data() {
+    return {
+      tab: 0
+    };
+  },
+  methods: {
+    changetab() {
+      // set tab to query
+      this.$router.push({query: {tab: this.tab}});
+    },
+  },
+  mounted() {
+    if (this.$route.query.tab) {
+      let tab = parseInt(this.$route.query.tab)
+      if (tab || tab === 0) {
+        this.tab = parseInt(this.$route.query.tab);
+      } else {
+        this.tab = this.$route.query.tab;
+      }
+    }
+  },
+};
+</script>
